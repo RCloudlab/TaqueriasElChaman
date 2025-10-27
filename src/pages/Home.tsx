2f4d640686392404d8c81d3990b510ba4b7af978
RCloudLab <rvesp04@hotmail.com>
2025-10-27 16:08:54 -0600

    feat(TC004): Initial image has been completed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,22 @@
+import { IMAGES } from "../constants/images"
+
 function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className=" flex-1 w-full mx-auto">
+      <picture>
+        <source 
+          media="(max-width: 768px)"
+          srcSet={IMAGES.tacosHomeMobile} 
+          type="image/png" 
+        />
+        <source srcSet={IMAGES.tacosHome} type="image/avif" />
+        <source srcSet={IMAGES.tacosHomeweb} type="image/webp" />
+        <img 
+          src={IMAGES.tacosHomejpg} 
+          alt="Plato de tacos al pastor y bistec de Taquerías El Chaman" 
+          className="w-full h-screen object-cover md:h-auto"
+        />
+      </picture>
     </div>
   )
 }
