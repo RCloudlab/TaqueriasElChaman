a0b551aa8c94769c27c706f84729c99bbf891396
ppambriz <ppambriz@gmail.com>
2025-10-28 00:33:47 -0600

    feat(TC002): header in progress

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/layout/Footer";
+import NavBar from "./components/layout/NavBar";
+import NavBarResponsive from "./components/layout/NavBarResponsive";
+
 function App() {
   return (
     <BrowserRouter>
-      {/* <NavBar /> */}
+       <NavBar />
+       <br />
+       <NavBarResponsive />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-      <Footer />
+      {<Footer />}
     </BrowserRouter>
   );
 }
