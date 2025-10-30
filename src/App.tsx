fc6a0740190beb1dc052f73cf5fc82c39bf29f0f
ppambriz <ppambriz@gmail.com>
2025-10-30 00:40:26 -0600

    feat(TC002):header second review

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,28 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/layout/Footer";
 import NavBar from "./components/layout/NavBar";
+import deerLogo from "./assets/Images/deer-logo.svg";
+import insta from "./assets/Images/Insta.jpg";
 
 function App() {
   return (
     <BrowserRouter>
-       <NavBar />
+      <NavBar />
+      <div>
+        {/* ESTE MAIN LO PUSE DE PRUEBA PARA MOSTRAR EL FIXED DEL HEADER */}
+        <main className="py-[600px] bg-black/60 flex flex-col gap-40 font-bold">
+          Main
+          <p>
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            voluptate eius dolor laboriosam illum quos illo exercitationem ea
+            molestiae veritatis, sapiente sunt obcaecati fugiat adipisci eum,
+            consequuntur accusantium, aliquam dolorum?
+          </p>
+          <img src={deerLogo} alt="Logo" />
+          <img src={insta} alt="Logo" />
+        </main>
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,8 +1,12 @@
+import { useState, useEffect } from "react";
 import { FaSquare } from "react-icons/fa";
-import { IoIosArrowForward } from "react-icons/io";
+import { IoMdMenu, IoMdClose } from "react-icons/io";
 import { IMAGES } from "../../constants/images";
 
 function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
   const address =
     "Tejedores de Aranza 512, Vasco de Quiroga, 58230 Morelia, Mich.";
   const query = encodeURIComponent(address);
@@ -26,32 +30,48 @@ function NavBar() {
     window.open(googleUrl, "_blank", "noopener");
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) setIsScrolled(true);
+      else setIsScrolled(false);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav className="w-full bg-black text-white">
-      <div className="flex flex-col sm:flex-row justify-between text-center sm:text-left">
-        <div className="flex items-center justify-center sm:justify-start gap-3 text-xs sm:text-sm py-2 flex-wrap sm:ml-16">
+    <nav
+      className={`w-full fixed top-0 left-0 z-50 transition-all duration-500 ${
+        isScrolled ? "bg-black/80 backdrop-blur-md shadow-md" : "bg-transparent"
+      } text-white px-8 py-1 lg:px-12`}
+    >
+      <div className="container mx-auto flex flex-col sm:flex-row justify-between text-center sm:text-left">
+        <div className="flex items-center justify-center sm:justify-start gap-4 text-ml sm:text-[12px] flex-wrap sm:ml-1">
           <p>Lun-Sab: 6:30pm - 11:30pm</p>
           <FaSquare className="text-gray-400 rotate-45 size-1.5" />
           <p>Domingos: 12pm - 11:30pm</p>
         </div>
 
-        <div className="flex items-center justify-center sm:justify-end gap-3 text-xs sm:text-sm py-2 flex-wrap sm:mr-16">
-          <p>taqueriaselchaman@gmail.com</p>
-          <FaSquare className="text-gray-400 rotate-45 size-1.5" />
-          <a
-            href="tel:+524431413799"
-            className="underline"
-            aria-label="Llamar a +52 443 141 3799"
-          >
-            +52 443 141 3799
-          </a>
+        <div className="hidden sm:flex items-center justify-end gap-4 text-sm py-2 flex-wrap sm:mr-1">
+          <div className="flex items-center gap-4">
+            <p>taqueriaselchaman@gmail.com</p>
+            <FaSquare className="text-gray-400 rotate-45 size-1.5" />
+            <a
+              href="tel:+524431413799"
+              className="underline"
+              aria-label="Llamar a +52 443 141 3799"
+            >
+              +52 443 141 3799
+            </a>
+          </div>
           <FaSquare className="text-gray-400 rotate-45 size-1.5" />
           <a
             href={googleUrl}
             onClick={openMaps}
             target="_blank"
             rel="noopener noreferrer"
-            className=""
+            className="text-sm text-left"
             aria-label="Abrir dirección en Google Maps"
           >
             {address}
@@ -59,31 +79,57 @@ function NavBar() {
         </div>
       </div>
 
-      <hr className="border-[#3A3940] my-1 mx-6 sm:mx-12" />
+      <hr className="border-[#3A3940] my-1 container mx-auto" />
 
-      <div className="flex flex-col sm:flex-row justify-between items-center px-4 sm:px-10 py-2">
-        <a
-          className="flex flex-col sm:flex-row items-center mb-3 sm:mb-0 text-center sm:text-left"
-          href="#"
-        >
+      <div className="container mx-auto flex items-center justify-between">
+        <a className="flex items-center gap-3" href="#">
           <img
             src={IMAGES.deerLogo}
             alt="Logo Taquerías el Chaman"
-            className="h-16 sm:h-20 object-contain mx-auto sm:mx-0"
+            className="h-16 sm:h-20 object-contain"
           />
-          <h1 className="text-2xl sm:text-4xl font-light sm:ml-2">
+          <h1 className="text-2xl sm:text-4xl font-light">
             Taquerías El Chaman
           </h1>
         </a>
 
-        <button className="border border-red-600 px-6 py-3 sm:px-9 sm:py-4 hover:bg-red-900/30 transition flex items-center gap-3">
-          <div className="flex flex-col gap-[5px]">
-            <span className="block w-5 sm:w-6 h-[1.5px] bg-white" />
-            <span className="block w-5 sm:w-6 h-[1.5px] bg-white" />
-          </div>
-          <IoIosArrowForward className="text-white text-lg sm:text-xl" />
+        <button
+          className="sm:hidden flex flex-col justify-center items-center gap-1 border border-red-600 px-4 py-2 rounded-lg hover:bg-red-900/30 transition"
+          onClick={() => setIsOpen(!isOpen)}
+          aria-label="Abrir menú"
+        >
+          {isOpen ? (
+            <IoMdClose className="text-white text-2xl" />
+          ) : (
+            <IoMdMenu className="text-white text-2xl" />
+          )}
         </button>
       </div>
+
+      {isOpen && (
+        <div className="sm:hidden flex flex-col items-start gap-4 bg-[#111] mt-4 p-4 rounded-lg text-sm">
+          <p>taqueriaselchaman@gmail.com</p>
+
+          <a
+            href="tel:+524431413799"
+            className="underline"
+            aria-label="Llamar a +52 443 141 3799"
+          >
+            +52 443 141 3799
+          </a>
+
+          <a
+            href={googleUrl}
+            onClick={openMaps}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-left break-words"
+            aria-label="Abrir dirección en Google Maps"
+          >
+            {address}
+          </a>
+        </div>
+      )}
     </nav>
   );
 }
