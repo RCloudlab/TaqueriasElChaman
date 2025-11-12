1cc43a783767990f329bad26cb692502af01f7d3
RCloudLab <rvesp04@hotmail.com>
2025-11-12 14:32:52 -0600

    fix(00): Fix Deploy

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,15 +1,15 @@
-import deerLogo from "../assets/images/deer-logo.svg";
-import insta from "../assets/images/Insta.jpg";
-import tacosHome from "../assets/images/home-image.avif";
-import tacosHomejpg from "../assets/images/home-image.jpg";
-import tacosHomeweb from "../assets/images/home-image.webp";
-import tacosHomeMobile from "../assets/images/home-image-mobile.png";
-import buttonMenu from "../assets/images/b-menu.gif";
-import burrito from "../assets/images/burrito-image.jpg";
-import groseria from "../assets/images/groseria-image.jpg";
-import tacos from "../assets/images/tacos-image.jpg";
-import totopos from "../assets/images/totopos-image.jpg";
-import volcan from "../assets/images/volcan-image.jpg";
+import deerLogo from "../assets/Images/deer-logo.svg";
+import insta from "../assets/Images/Insta.jpg";
+import tacosHome from "../assets/Images/home-image.avif";
+import tacosHomejpg from "../assets/Images/home-image.jpg";
+import tacosHomeweb from "../assets/Images/home-image.webp";
+import tacosHomeMobile from "../assets/Images/home-image-mobile.png";
+import buttonMenu from "../assets/Images/b-menu.gif";
+import burrito from "../assets/Images/burrito-image.jpg";
+import groseria from "../assets/Images/groseria-image.jpg";
+import tacos from "../assets/Images/tacos-image.jpg";
+import totopos from "../assets/Images/totopos-image.jpg";
+import volcan from "../assets/Images/volcan-image.jpg";
 
 import diamod from "../assets/icons/diamod.svg";
 
