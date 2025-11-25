358d1b4cb400b8c7c5d0ec9450a11c74d06eb43f
Sergio Cardona <checocardona01@gmail.com>
2025-11-25 01:59:07 -0600

    fix(TC013): Correción de los botones, cambiado e implementado de las direcciones

--- src/components/layout/Collaborations.tsx
+++ src/components/layout/Collaborations.tsx
@@ -7,8 +7,20 @@ function Collaborations() {
         <section className="relative w-full py-20 px-5 bg-white overflow-hidden">
             <div className="relative z-20 max-w-6xl mx-auto">
                 <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-6 mb-4">
-                    <a href="#" className="w-full md:w-auto text-center bg-[#C00F0C] text-white px-12 py-2 rounded-md text-sm hover:bg-red-800 transition-colors">Contactar</a>
-                    <a href="#" className="w-full md:w-auto text-center border border-[#C00F0C] px-12 py-2 rounded-md text-sm hover:bg-[#C00F0C] hover:text-white transition-colors">Ver todos los videos</a>
+                    <a
+                        href="mailto:taqueriasaelchaman@gmail.com"
+                        className="w-full md:w-auto text-center bg-[#C00F0C] text-white px-12 py-2 rounded-md text-sm hover:bg-red-800 transition-colors"
+                    >
+                        Contactar
+                    </a>
+                    <a
+                        href="https://www.tiktok.com/@taqueriaselchaman"  
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="w-full md:w-auto text-center border border-[#C00F0C] px-12 py-2 rounded-md text-sm hover:bg-[#C00F0C] hover:text-white transition-colors"
+                    >
+                        Ver todos los videos
+                    </a>
                 </div>
                 <div>
                     <h2 className="text-3xl md:text-4xl font-bold text-gray-900 uppercase pb-6 md:text-left text-center">
