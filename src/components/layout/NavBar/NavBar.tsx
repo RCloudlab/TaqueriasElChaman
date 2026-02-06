fb1a71ef83fee46f87cf0dc27afab24d1575a31b
ppambriz <ppambriz@gmail.com>
2026-02-05 23:02:51 -0600

    fix/T018-NavBar:Mejoras

--- src/components/layout/NavBar/NavBar.tsx
+++ src/components/layout/NavBar/NavBar.tsx
@@ -1,14 +1,9 @@
 import { useState, useEffect } from "react";
-import { ItemsContact } from "./ItemsContact";
 import { ItemsNav } from "./ItemsNav";
 import { IMAGES } from "../../../constants/images";
 import { GiHamburgerMenu } from "react-icons/gi";
-import {
-  schedules,
-  contactInformation,
-  optionsNav,
-} from "../../../constants/navbarConstant";
-import type { ContactInformation, OptionsNav } from "./props";
+import { optionsNav } from "../../../constants/navbarConstant";
+import type { OptionsNav } from "./props";
 import { IoClose } from "react-icons/io5";
 
 export const NavBar = () => {
@@ -60,34 +55,12 @@ export const NavBar = () => {
   return (
     <nav
       className={`flex flex-col w-full fixed top-0 left-0 right-0 z-40 transition-all duration-500 ${
-        isScrolled ? "bg-black/80 backdrop-blur-md shadow-md" : "bg-transparent"
-      } text-white px-8 py-2 lg:px-12`}
+        isScrolled
+          ? "bg-black/80 backdrop-blur-md shadow-md"
+          : "bg-transparent py-8"
+      } text-white px-8 py-3 lg:px-12`}
     >
       <div className="container mx-auto relative py-2">
-        {/**Information Section */}
-        <div className="hidden md:block lg:flex justify-between text-xs pb-2 space-y-1 lg:space-y-0">
-          {/**schedules */}
-          <div className="flex justify-between space-x-5">
-            {schedules.map((schedule: ContactInformation) => (
-              <ItemsContact key={schedule.id} {...schedule} />
-            ))}
-          </div>
-
-          {/**Phone, Address */}
-          <div className="grid grid-rows-2 md:gap-4 md:flex md:justify-between">
-            {contactInformation.map((contact: ContactInformation) => (
-              <ItemsContact
-                styles="justify-star pl-0 space-y-1"
-                key={contact.id}
-                {...contact}
-              />
-            ))}
-          </div>
-        </div>
-
-        {/**Dividing line */}
-        <hr className="hidden md:block border-[#3A3940] my-2 container mx-auto" />
-
         {/**Nav section */}
         <div className="flex justify-between -my-2 items-center md:pt-2">
           {/**Logo, name */}
@@ -96,16 +69,16 @@ export const NavBar = () => {
               <img
                 src={IMAGES.deerLogo}
                 alt="Logo Taqueria Chaman"
-                className="h-16 object-contain md:-my-2 mt-2"
+                className="size-16 md:-my-2 mt-2"
               />
             </a>
             <a href="/">
-              <h1 className="md:text-2xl text-lg">Taquerías El Chaman</h1>
+              <h1 className="text-2xl">Taquerías El Chaman</h1>
             </a>
           </div>
 
           {/**NAV */}
-          <div className="hidden lg:flex space-x-6 text-center max-lg:space-x-2">
+          <div className="hidden md:flex space-x-6 text-center max-lg:space-x-2">
             {optionsNav.map((option: OptionsNav) => (
               <ItemsNav key={option.id} {...option} />
             ))}
@@ -128,24 +101,25 @@ export const NavBar = () => {
 
           <button
             onClick={() => setIsOpen(!isOpen)}
-            className="hidden ml-20 max-lg:flex md:px-4 text-3xl text-red-900 hover:text-red-900/80 transition-all duration-400"
+            className="hidden ml-20 max-md:flex md:px-4 text-5xl text-red-900 hover:text-red-900/80 transition-all duration-400"
           >
             {isOpen ? <IoClose /> : <GiHamburgerMenu />}
           </button>
         </div>
       </div>
       {/**Buttons mobile */}
-      <div
-        className={`fex text-center mt-2 ${isOpen ? "-mb-[128px]" : "mb-0"}`}
-      >
+      <div className={`flex m-auto ${isOpen ? "-mb-[12px]" : "mb-0"}`}>
         <div
           className={`${
-            isOpen ? "block" : "hidden"
-          } block lg:hidden w-[100vw] bg-gray-600/80 text-gray-750 rounded-b-md text-md -ml-8`}
+            isOpen ? "block py-4 text-center sm:text-left" : "hidden"
+          } block lg:hidden w-[100vw] bg-red-900/10 text-gray-750 rounded-b-md text-md -ml-8`}
         >
           {optionsNav.map((option: OptionsNav) => (
-            <ItemsNav styles="" key={option.id} {...option} />
+            <ItemsNav styles="ml-14" key={option.id} {...option} />
           ))}
+          <a className=" ml-14" href={googleUrl} target="_blank">
+            Ubicacion
+          </a>
         </div>
       </div>
     </nav>
